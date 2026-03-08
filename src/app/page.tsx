b60528bf712f17195184dee2dad799722d396ea6
sleduardo20 <sl.eduardo20@gmail.com>
2026-03-07 22:36:21 -0300

    create components sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Sidebar } from "@/components/sidebar";
+
 export default function Home() {
   return (
     <section className="flex min-h-full">
+      <Sidebar />
       <div className="flex flex-1 items-center justify-center">
         <div className="text-center">
           <h1 className="text-2xl font-bold mb-4">Selecione um prompt</h1>
---
+++ src/components/sidebar/sidebar-content.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useState } from "react";
+import { Prompt } from "@/generated/prisma/browser";
+import { Button } from "../ui/button";
+import {
+  ArrowLeftToLine,
+  X as CloseButton,
+  Plus as AddIcon,
+  ArrowRightToLine,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Logo } from "../logo";
+
+export const SidebarContent = (prompts: Prompt[]) => {
+  const router = useRouter();
+  const [isCollapsed, setIsCollapset] = useState(false);
+
+  const collapsedSidebar = () => setIsCollapset(true);
+  const expandSidebar = () => setIsCollapset(false);
+  const handleNewPrompt = () => router.push("/new");
+  return (
+    <aside
+      className={`border-r border-gray-700 flex flex-col h-full bg-gray-800 
+    transition-[transform,width] duration-300 ease-in-out fixed  left-0 top-0 z-50 md:z-auto w-[80vw] sm:w-[320px] ${isCollapsed ? "md:w-[72px]" : "md:w-[384px]"}`}
+    >
+      {isCollapsed && (
+        <section className="px-2 py-6">
+          <header className="flex items-center justify-center mb-6">
+            <Button
+              onClick={expandSidebar}
+              variant={"icon"}
+              className="hidden md:inline-flex p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-accent-500 rounded-lg transition-colors "
+              title="Expandir sidebar"
+              aria-label="Expandir sidebar"
+            >
+              <ArrowRightToLine className="w-5 h-5 text-gray-100" />
+            </Button>
+          </header>
+        </section>
+      )}
+
+      {!isCollapsed && (
+        <>
+          <section className="p-6">
+            <div className="md:hidden mb-4">
+              <div className="flex items-center justify-between">
+                <Button
+                  variant="secondary"
+                  aria-label="Fechar menu"
+                  title="Fechar menu"
+                >
+                  <CloseButton className="w-5 h-5 text-gray-100" />
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex w-full items-center justify-between mb-6">
+              <header className="flex w-full items-center justify-between">
+                <Logo />
+                <Button
+                  onClick={collapsedSidebar}
+                  variant={"icon"}
+                  className="hidden md:inline-flex p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 
+                  focus:ring-accent-500 rounded-lg transition-colors"
+                >
+                  <ArrowLeftToLine className="w-5 h-5 text-gray-100" />
+                </Button>
+              </header>
+            </div>
+
+            <div className="">
+              <Button onClick={handleNewPrompt} className="w-full" size={"lg"}>
+                <AddIcon className="w-5 h-5 mr-2" />
+                Novo prompt
+              </Button>
+            </div>
+          </section>
+        </>
+      )}
+    </aside>
+  );
+};
